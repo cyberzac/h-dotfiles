c5769a4dcd4d9543ff9cbfa318ab49e0b6de127d
Martin Zachrison <zac@cyberzac.se>
2014-11-14 11:23:06 +0100

    java8 and WALLET_DIR

--- strawpay/env.zsh
+++ strawpay/env.zsh
@@ -1,4 +1,4 @@
 # Log Entries token for my machine, twin and shub
-export WALLET_DIRECTORY=.
+export WALLET_DIRECTORY=/Users/zac/Code/shub/wallets
 export LE_TOKEN=3bcf37b7-c7ae-4fae-a115-f339f16f5dcf
 
