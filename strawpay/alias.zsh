14c1c983f920d58baaeb762baacf6cd639064153
cyberzac <zac@cyberzac.se>
2015-05-20 10:13:45 +0200

    Added franc ssh alias

--- strawpay/alias.zsh
+++ strawpay/alias.zsh
@@ -1,8 +1,6 @@
 
 alias strawpay-mvn="(cd ~/.m2; ln -shf strawpay.xml settings.xml)"
-alias docgen-placement="(cd ~/Code/docgen; sbt run; open 'Private Placement Memorandum.pdf')
-
-
-"
+alias docgen-placement="(cd ~/Code/docgen; sbt run; open 'Private Placement Memorandum.pdf')"
+alias franc="ssh franc.strawpay.com $*"
 
 
