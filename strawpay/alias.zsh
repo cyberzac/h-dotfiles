0679355e98be551a28c74252cc00a02d9848faef
cyberzac <zac@cyberzac.se>
2015-06-29 20:33:07 +0200

    Added mark

--- strawpay/alias.zsh
+++ strawpay/alias.zsh
@@ -3,7 +3,8 @@ alias strawpay-mvn="(cd ~/.m2; ln -shf strawpay.xml settings.xml)"
 alias docgen-placement="(cd ~/Code/docgen; sbt run; open 'Private Placement Memorandum.pdf')"
 alias franc="ssh franc.strawpay.com $*"
 alias daler="ssh daler.strawpay.com $*"
-alias krona="ssh krona.strawpay.com $*"
 alias dollar="ssh dollar.strawpay.com $*"
+alias krona="ssh krona.strawpay.com $*"
+alias mark="ssh mark.strawpay.com $*"
 
 
