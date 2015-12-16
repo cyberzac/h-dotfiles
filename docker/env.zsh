4f5242ee463756fba37a1b2130f06a7c5e1a375d
cyberzac <zac@cyberzac.se>
2015-12-16 23:42:19 +0100

    Removed running och docker env

--- docker/env.zsh
+++ docker/env.zsh
@@ -1,6 +1,6 @@
 # Run this command to configure your shell:
-eval "$(docker-machine env dev)"
-#export DOCKER_TLS_VERIFY="1"
-#export DOCKER_HOST="tcp://192.168.99.100:2376"
-#export DOCKER_CERT_PATH="/Users/zac/.docker/machine/machines/dev"
-#export DOCKER_MACHINE_NAME="dev"
+#eval "$(docker-machine env dev)"
+export DOCKER_TLS_VERIFY="1"
+export DOCKER_HOST="tcp://192.168.99.100:2376"
+export DOCKER_CERT_PATH="/Users/zac/.docker/machine/machines/dev"
+export DOCKER_MACHINE_NAME="dev"
