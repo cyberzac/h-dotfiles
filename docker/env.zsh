57705e93125f9b4712f4918012798583669fa75d
cyberzac <zac@cyberzac.se>
2016-04-28 09:24:31 +0200

    REmoved setting och docker env as it conflicts with Docker for Mac

--- docker/env.zsh
+++ docker/env.zsh
@@ -1,6 +1,11 @@
 # Run this command to configure your shell:
 #eval "$(docker-machine env dev)"
-export DOCKER_TLS_VERIFY="1"
-export DOCKER_HOST="tcp://192.168.99.100:2376"
-export DOCKER_CERT_PATH="/Users/zac/.docker/machine/machines/dev"
-export DOCKER_MACHINE_NAME="dev"
+# Removed to allow for the Docker for Mac
+#export DOCKER_TLS_VERIFY="1"
+#export DOCKER_HOST="tcp://192.168.99.100:2376"
+#export DOCKER_CERT_PATH="/Users/zac/.docker/machine/machines/dev"
+#export DOCKER_MACHINE_NAME="dev"
+unset DOCKER_TLS_VERIFY
+unset DOCKER_HOST
+unset DOCKER_CERT_PATH
+unset DOCKER_MACHINE_NAME
