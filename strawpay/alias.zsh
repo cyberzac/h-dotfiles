5c90cade2badfae60a105c9a3837f10758422ca8
cyberzac <zac@cyberzac.se>
2016-10-24 20:07:46 +0200

    Added krona

--- strawpay/alias.zsh
+++ strawpay/alias.zsh
@@ -4,10 +4,11 @@ alias docgen-placement="(cd ~/Code/docgen; sbt run; open 'Private Placement Memo
 
 alias franc="ssh franc.strawpay.com $*"
 alias daler="ssh daler.strawpay.com $*"
-alias dollar="ssh dollar.strawpay.com $*"
-alias krona="ssh krona.strawpay.com $*"
+alias dollar="ssh dollar.do.strawpay.com $*"
+alias krona="ssh krona.do.strawpay.com $*"
 alias dmark="ssh dmark.strawpay.com $*"
 alias dublon="ssh dublon.strawpay.com $*"
 alias escudo="ssh escudo.strawpay.com $*"
 alias schilling="ssh schilling.strawpay.com $*"
+alias ringit="ssh ringit.strawpay.com $*"
 
