d4998ebf19f500aa6f0c6b12e895afbea5c01be8
Martin Zachrison <zac@cyberzac.se>
2019-11-14 11:29:03 +0100

    Youmoni server alias

--- youmoni/alias.zsh
+++ youmoni/alias.zsh
@@ -3,6 +3,7 @@ alias ym-dev01="ssh kuriren34.dyndns-ip.com"
 alias ym-dev02="ssh -p 24 kuriren34.dyndns-ip.com"
 alias kuriren="ssh kuriren34.dyndns-ip.com"
 alias ym-test="ssh manager01.test.youmoni.net"
-alias ym-dev="ssh manager01.dev.youmoni.net"
+alias ym-test="ssh manager01.test.youmoni.net"
+alias ym-prod="ssh manager01.youmoni.net"
 alias influxadm="ssh influxadm"
 
