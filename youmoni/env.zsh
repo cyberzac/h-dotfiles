e2497bef9f2e35200a0d3e7b8da31e98c73c99ba
Martin Zachrison <martin@kodmagi.se>
2024-04-15 11:29:19 +0200

    AWS via 1Password

--- youmoni/env.zsh
+++ youmoni/env.zsh
@@ -1,3 +1,7 @@
 #
 export YOUMONI_ENVIRONMENT=local
 export INFLUX_EVENTLOG_DIR=./target/influxLog
+export AWS_ACCESS_KEY_ID=AKIAVNLRZDYJ2IEJBI65
+export AWS_SECRET_ACCESS_KEY="op://Private/aws s3 dev/password"
+
+
